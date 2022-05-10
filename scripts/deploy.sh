aac831a46b797a988e9b87ceb59e197f29a5cb32
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2022-05-10 16:20:52 -0400

    Remove client build

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,6 @@
 git push origin main
-ssh $DROPLET_USER@$DROPLET_IP "cd /home/taylormitchell/code/quick-capture/src && git pull origin main && npm run build"
+ssh $DROPLET_USER@$DROPLET_IP \
+    "cd /home/taylormitchell/code/quick-capture/src && "\
+    "git pull origin main && "\
+    "PATH=/home/taylormitchell/.nvm/versions/node/v17.7.1/bin:$PATH && "\
+    "npm run build"
