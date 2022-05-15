2d131f4f5e490ec5dab4d8b5ebbcb6c4e03582b0
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2022-05-15 00:22:34 -0400

    Fix inbox action buttons

--- src/client/src/components/Inbox.tsx
+++ src/client/src/components/Inbox.tsx
@@ -13,13 +13,13 @@ const Inbox = (props: Props) => {
   const { inboxNotes } = props;
   const currentNote = inboxNotes.slice(-1)[0];
 
-  const keep = () => props.updateNote(currentNote.id, model.keep(currentNote));
-  const archive = () => props.updateNote(currentNote.id, model.archive(currentNote));
-  const skip = () => props.updateNote(currentNote.id, {...currentNote, dueDate: Date.now() + 5 * 60 * 1000});
-  let quickActions = (
+  const keep = (id: string) => props.updateNote(id, model.keep(currentNote));
+  const archive = (id: string) => props.updateNote(id, model.archive(currentNote));
+  // const skip = (id) => props.updateNote(currentNote.id, {...currentNote, dueDate: Date.now() + 5 * 60 * 1000});
+  let quickActions = (id: string) => (
     <div className="quick-actions">
-      <button onClick={keep}>Keep</button>
-      <button onClick={archive}>Archive</button>
+      <button onClick={() => keep(id)}>Keep</button>
+      <button onClick={() => archive(id)}>Archive</button>
       {/* <button onClick={skip}>Skip</button> */}
     </div>
   )
@@ -32,7 +32,7 @@ const Inbox = (props: Props) => {
       {props.inboxNotes
         .sort((a, b) => a.dueDate - b.dueDate)
         .map((n) => (
-          <Card key={n.id} note={n} updateNote={props.updateNote} quickActions={quickActions}/>
+          <Card key={n.id} note={n} updateNote={props.updateNote} quickActions={quickActions(n.id)}/>
         ))}
     </div>
   );
