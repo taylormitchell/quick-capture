e2e943b22510502175bed0fbeac5021376fe85c0
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2022-05-31 13:48:28 -0400

    Remove click-to-edit card text

--- src/client/src/components/Card.tsx
+++ src/client/src/components/Card.tsx
@@ -11,50 +11,13 @@ type CardProps = {
 
 function Card(props: CardProps) {
   const { note } = props;
-  const update = (values: Partial<Note>) => {
-    props.applyToNote(note.id, (n) => ({ ...n, ...values }));
-  }
-
-  const [textEditing, setTextEditing] = useState<string>("");
-  const [textEditable, setTextEditable] = useState<boolean>(false);
-  const textArea = useRef<HTMLTextAreaElement>(null);
   const [showDetails, setShowDetails] = useState<boolean>(false);
-
-  // Focus when selected
-  useEffect(() => {
-    let el = textArea.current;
-    if (el) {
-      const end: number = el.value.length;
-      el.focus();
-      el.setSelectionRange(end, end);
-    }
-  }, [textEditable]);
-
-  const textFocusHandler = () => {
-    setTextEditable(true);
-    setTextEditing(note.text);
-  };
-
-  const textBlurHandler = () => {
-    update({ text: textEditing });
-    setTextEditable(false);
-    setTextEditing("");
-  };
 
   return (
     <div id={note.id} className="card">
       <main>
         <section>
-          {textEditable ? (
-            <textarea
-              ref={textArea}
-              value={textEditing}
-              onChange={(e) => setTextEditing(e.target.value)}
-              onBlur={textBlurHandler}
-            ></textarea>
-          ) : (
-            <p onClick={textFocusHandler}> {note.text} </p>
-          )}
+          <p>{note.text}</p>
         </section>
         <footer>
           <Details details={showDetails ? note : { createdDate: note.createdDate }} />
@@ -72,10 +35,6 @@ function Card(props: CardProps) {
           </div> */}
         </footer>
       </main>
-      {/* <aside>
-          <button onClick={keep}>Keep</button>
-          <button onClick={archive}>Archive</button>
-      </aside> */}
     </div>
   );
 }
